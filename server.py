43b5cbadf9b9d15c005bc57f19d69664e90eb2f0
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-14 20:07:22 +0000

    add d link from root to /hello

--- server.py
+++ server.py
@@ -18,7 +18,13 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+    Hi! This is the home page.
+    <a href="/hello">Go to hello.</a>
+    </html>
+    """
 
 
 @app.route('/hello')
